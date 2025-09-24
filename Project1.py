af5f871255eed29a9ac321ee8fa805203ebea84b
AidanMizi <127631159+AidanMizi@users.noreply.github.com>
2025-09-23 20:30:40 -0400

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -1,12 +1,27 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 23 20:23:45 2025
 
-@author: aidan
-"""
+# AER850 PROJECT 1 FALL 2025
+# AIDAN MIZIOLEK
 
-# PROJECT 1 
+
+# imports
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+
+# read data
+data = pd.read_csv("Project 1 Data.csv")
+
+
+
 
 
 
